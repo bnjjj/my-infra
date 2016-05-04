4e7eb7476d809270fd07ff29eafecd0a1a05fd94
Benjamin Coenen <benjamin.coenen@hotmail.com>
2016-05-04 20:15:23 +0200

    chore(upgrade version of ionic, up to beta 6):

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import {App, Platform} from 'ionic-angular';
+import {Keyboard} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
 import {provide} from 'angular2/core';
@@ -63,20 +64,8 @@ export class MyApp {
         event.preventDefault();
         event.stopPropagation();
       }, false);
-      // The platform is now ready. Note: if this callback fails to fire, follow
-      // the Troubleshooting guide for a number of possible solutions:
-      //
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      //
-      // First, let's hide the keyboard accessory bar (only works natively) since
-      // that's a better default:
-      //
-      // Keyboard.setAccessoryBarVisible(false);
-      //
-      // For example, we might change the StatusBar color. This one below is
-      // good for dark backgrounds and light text:
-      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+
+      Keyboard.hideKeyboardAccessoryBar(false);
     });
   }
 }
---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+var gulp = require('gulp'),
+    gulpWatch = require('gulp-watch'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
+    argv = process.argv;
+
+
+/**
+ * Ionic hooks
+ * Add ':before' or ':after' to any Ionic project command name to run the specified
+ * tasks before or after the command.
+ */
+gulp.task('serve:before', ['watch']);
+gulp.task('emulate:before', ['build']);
+gulp.task('deploy:before', ['build']);
+gulp.task('build:before', ['build']);
+
+// we want to 'watch' when livereloading
+var shouldWatch = argv.indexOf('-l') > -1 || argv.indexOf('--livereload') > -1;
+gulp.task('run:before', [shouldWatch ? 'watch' : 'build']);
+
+/**
+ * Ionic Gulp tasks, for more information on each see
+ * https://github.com/driftyco/ionic-gulp-tasks
+ *
+ * Using these will allow you to stay up to date if the default Ionic 2 build
+ * changes, but you are of course welcome (and encouraged) to customize your
+ * build however you see fit.
+ */
+var buildBrowserify = require('ionic-gulp-browserify-typescript');
+var buildSass = require('ionic-gulp-sass-build');
+var copyHTML = require('ionic-gulp-html-copy');
+var copyFonts = require('ionic-gulp-fonts-copy');
+var copyScripts = require('ionic-gulp-scripts-copy');
+
+var isRelease = argv.indexOf('--release') > -1;
+
+gulp.task('watch', ['clean'], function(done){
+  runSequence(
+    ['sass', 'html', 'fonts', 'scripts', 'img'],
+    function(){
+      gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
+      gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
+      buildBrowserify({ watch: true }).on('end', done);
+    }
+  );
+});
+
+gulp.task('build', ['clean'], function(done){
+  runSequence(
+    ['sass', 'html', 'fonts', 'scripts', 'img'],
+    function(){
+      buildBrowserify({
+        minify: isRelease,
+        browserifyOptions: {
+          debug: !isRelease
+        },
+        uglifyOptions: {
+          mangle: false
+        }
+      }).on('end', done);
+    }
+  );
+});
+
+gulp.task('img', function () {
+  return gulp.src(['./resources/img/**/*'])
+      .pipe(gulp.dest('www/build/img'));
+});
+
+gulp.task('sass', function () {
+  buildSass({src: [
+    'app/theme/app.+(ios|md|wp).scss',
+    'node_modules/animate.css/animate.min.css',
+    'node_modules/font-awesome/scss/font-awesome.scss'
+  ]});
+});
+gulp.task('html', copyHTML);
+gulp.task('fonts', function () {
+  copyFonts({
+    src: [
+      'node_modules/ionic-angular/fonts/**/*.+(ttf|woff|woff2)',
+      'node_modules/font-awesome/fonts/**/*.+(ttf|woff|woff2)'
+    ]
+  });
+});
+gulp.task('scripts', copyScripts);
+gulp.task('clean', function(){
+  return del('www/build');
+});
