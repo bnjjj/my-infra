623dc5700aa7e0e4822e718ba66862c48ecd03c1
Antoine Leblanc <ant.leblanc@gmail.com>
2016-11-06 15:41:45 +0100

    fix(sms): available now on project.

--- app/components/widgets/project-widget/project-widget.ts
+++ app/components/widgets/project-widget/project-widget.ts
@@ -1,21 +1,27 @@
-import {WidgetsService} from '../widgets.service';
-import {Component, Input, Output, EventEmitter, ViewChild} from '@angular/core';
-import {AnalyticsService} from '../../../services/analytics/analytics.service';
-import {IONIC_DIRECTIVES, Nav} from 'ionic-angular';
-import {WebWidgetContentComponent} from '../web-widget/content/web-widget-content';
-import {PrivateDatabaseWidgetContentComponent} from '../private-database-widget/content/private-database-widget-content';
-import {DomainWidgetContentComponent} from '../domain-widget/content/domain-widget-content';
-import {DedicatedWidgetContentComponent} from '../dedicated-widget/content/dedicated-widget-content';
-import {VpsWidgetContentComponent} from '../vps-widget/content/vps-widget-content';
-import {CloudWidgetContentComponent} from '../cloud-widget/content/cloud-widget-content';
-import {categoryEnum} from '../../../config/constants';
+import { WidgetsService } from '../widgets.service';
+import { Component, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import { AnalyticsService } from '../../../services/analytics/analytics.service';
+import { IONIC_DIRECTIVES, Nav } from 'ionic-angular';
+import { WebWidgetContentComponent } from '../web-widget/content/web-widget-content';
+import { PrivateDatabaseWidgetContentComponent } from '../private-database-widget/content/private-database-widget-content';
+import { DomainWidgetContentComponent } from '../domain-widget/content/domain-widget-content';
+import { DedicatedWidgetContentComponent } from '../dedicated-widget/content/dedicated-widget-content';
+import { VpsWidgetContentComponent } from '../vps-widget/content/vps-widget-content';
+import { SmsWidgetContentComponent } from '../sms-widget/content/sms-widget-content';
+import { CloudWidgetContentComponent } from '../cloud-widget/content/cloud-widget-content';
+import { categoryEnum } from '../../../config/constants';
 
 @Component({
   selector: 'project-widget',
   templateUrl: 'build/components/widgets/project-widget/project-widget.html',
-  directives: [IONIC_DIRECTIVES, WebWidgetContentComponent,
-    DomainWidgetContentComponent, DedicatedWidgetContentComponent,
-    VpsWidgetContentComponent, PrivateDatabaseWidgetContentComponent,
+  directives: [
+    IONIC_DIRECTIVES,
+    WebWidgetContentComponent,
+    DomainWidgetContentComponent,
+    DedicatedWidgetContentComponent,
+    VpsWidgetContentComponent,
+    PrivateDatabaseWidgetContentComponent,
+    SmsWidgetContentComponent,
     CloudWidgetContentComponent
   ],
   providers: [WidgetsService]
